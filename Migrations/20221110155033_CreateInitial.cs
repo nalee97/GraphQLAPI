209560787e08d594b4d5984968309e884fef385e
nalee97 <mohamednaleef1564@gmail.com>
2022-11-10 21:24:27 +0530

    commit

---
+++ Migrations/20221110155033_CreateInitial.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQLAPI.Migrations
+{
+    public partial class CreateInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Superheroes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Height = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Superheroes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Instructor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SuperheroId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movies_Superheroes_SuperheroId",
+                        column: x => x.SuperheroId,
+                        principalTable: "Superheroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Superpowers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SuperPower = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SuperheroId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Superpowers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Superpowers_Superheroes_SuperheroId",
+                        column: x => x.SuperheroId,
+                        principalTable: "Superheroes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Superheroes",
+                columns: new[] { "Id", "Description", "Height", "Name" },
+                values: new object[] { new Guid("67a91459-acaf-4e59-ad14-aa8487733190"), "Batman is a superhero who appears in American comic books published by DC Comics. The character was created by artist Bob Kane and writer Bill Finger, and first appeared in Detective Comics #27 (May 1939). Originally", "6'2", "Batman" });
+
+            migrationBuilder.InsertData(
+                table: "Superheroes",
+                columns: new[] { "Id", "Description", "Height", "Name" },
+                values: new object[] { new Guid("b3f31f5c-63df-4336-999a-2efb21a02106"), "Superman is a fictional superhero appearing in American comic books published by DC Comics. The character was created by writer Jerry Siegel and artist Joe Shuster, high school students living in Cleveland, Ohio, in 1933.", "6'3", "Superman" });
+
+            migrationBuilder.InsertData(
+                table: "Superheroes",
+                columns: new[] { "Id", "Description", "Height", "Name" },
+                values: new object[] { new Guid("dc542550-e9fc-4f08-9f1f-374afb04138d"), "Luke Skywalker is a fictional character and the main protagonist of the original film trilogy of the Star Wars franchise created by George Lucas. He is portrayed by Mark Hamill in the original trilogy and by Hayden Christensen in the prequel trilogy.", "5'11", "Luke Skywalker" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Description", "Instructor", "ReleaseDate", "SuperheroId", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("109324dc-2535-4224-acd4-079939ec1f48"), "The Dark Knight is a 2008 superhero film directed, produced, and co-written by Christopher Nolan. Based on the DC Comics character Batman, the film is the second installment of Nolan's The Dark Knight Trilogy and a sequel to 2005's Batman Begins, starring Christian Bale and supported by Michael Caine, Heath Ledger, Gary Oldman, Aaron Eckhart, Maggie Gyllenhaal, and Morgan Freeman.", "Christopher Nolan", new DateTime(2008, 7, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("67a91459-acaf-4e59-ad14-aa8487733190"), "The Dark Knight" },
+                    { new Guid("68fccad3-963c-4e68-8f35-acb001b262d9"), "The Dark Knight Rises is a 2012 superhero film directed by Christopher Nolan, who co-wrote the screenplay with his brother Jonathan Nolan, and the story with David S. Goyer.", "Christopher Nolan", new DateTime(2012, 7, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("67a91459-acaf-4e59-ad14-aa8487733190"), "The Dark Knight Rises" },
+                    { new Guid("801c203c-0b1a-4c36-959c-4fb4d49b127c"), "Return of the Jedi (also known as Star Wars: Episode VI – Return of the Jedi) is a 1983 American epic space opera film directed by Richard Marquand. The screenplay is by Lawrence Kasdan and George Lucas from a story by Lucas, who was also the executive producer.", "Richard Marquand", new DateTime(1983, 5, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("b3f31f5c-63df-4336-999a-2efb21a02106"), "Star Wars: Episode VI – Return of the Jedi" },
+                    { new Guid("88b392dd-3279-4a77-bbfa-74c6ea138584"), "Star Wars (retroactively titled Star Wars: Episode IV – A New Hope) is a 1977 American epic space opera film written and directed by George Lucas, produced by Lucasfilm and distributed by 20th Century Fox.", "George Lucas", new DateTime(1977, 5, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("b3f31f5c-63df-4336-999a-2efb21a02106"), "Star Wars: Episode IV – A New Hope" },
+                    { new Guid("8fd99aef-afa3-4a64-a2e1-92026b982686"), "Black Widow is a 2021 American superhero film based on Marvel Comics featuring the character of the same name. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, it is the 24th film in the Marvel Cinematic Universe (MCU).", "Cate Shortland", new DateTime(2021, 6, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("dc542550-e9fc-4f08-9f1f-374afb04138d"), "Black Widow" },
+                    { new Guid("dfd1f97a-8c03-4ac4-90fb-2f4e1f39749f"), "The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner and written by Leigh Brackett and Lawrence Kasdan, based on a story by George Lucas.", "Irvin Kershner", new DateTime(1980, 5, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("b3f31f5c-63df-4336-999a-2efb21a02106"), "Star Wars: Episode V – The Empire Strikes Back" },
+                    { new Guid("f7b8bebf-779c-460f-bc30-837e77ff74e8"), "Batman Begins is a 2005 superhero film directed by Christopher Nolan and written by Nolan and David S. Goyer. Based on the DC Comics character Batman, it stars Christian Bale as Bruce Wayne / Batman, with Michael Caine, Liam Neeson, Katie Holmes, Gary Oldman,", "Christopher Nolan", new DateTime(2005, 6, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("67a91459-acaf-4e59-ad14-aa8487733190"), "Batman Begins" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Superpowers",
+                columns: new[] { "Id", "Description", "SuperPower", "SuperheroId" },
+                values: new object[,]
+                {
+                    { new Guid("0a907fe2-96b8-4dc6-88fe-a4d62cff68f2"), "Skywalker is able to deflect fire from a blaster back at the opponent firing. This enables Luke to turn someone else's weapon against them.", "Deflect blaster power.", new Guid("b3f31f5c-63df-4336-999a-2efb21a02106") },
+                    { new Guid("5da6cbf6-e376-473e-add5-b6d5d1bcae12"), "The knowledge of how to undermine others.", "Subterfuge", new Guid("dc542550-e9fc-4f08-9f1f-374afb04138d") },
+                    { new Guid("5f8ffd0c-5493-4fc0-802e-99d72ff0277e"), "She knows how to infiltrate the enemy.", "Infiltration", new Guid("dc542550-e9fc-4f08-9f1f-374afb04138d") },
+                    { new Guid("a1b6c31e-c2d6-45e1-a3ce-0e8a4ca31fe1"), "He got a lot of money", "Wealth.", new Guid("67a91459-acaf-4e59-ad14-aa8487733190") },
+                    { new Guid("b40f7cab-9826-4b3a-818a-d277638edc39"), "She's good at spying at people.", "Espionage", new Guid("dc542550-e9fc-4f08-9f1f-374afb04138d") },
+                    { new Guid("de133e85-a46e-4dc7-af6d-50fe20385221"), "Sublime fighting skills.", "Fighting", new Guid("67a91459-acaf-4e59-ad14-aa8487733190") },
+                    { new Guid("e35dfd2a-2a7f-48da-bca3-fba1a092f51a"), "He's always a step ahead.", "Intellect.", new Guid("67a91459-acaf-4e59-ad14-aa8487733190") }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_SuperheroId",
+                table: "Movies",
+                column: "SuperheroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Superpowers_SuperheroId",
+                table: "Superpowers",
+                column: "SuperheroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Superpowers");
+
+            migrationBuilder.DropTable(
+                name: "Superheroes");
+        }
+    }
+}
